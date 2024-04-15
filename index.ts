05838ea6df2e41712059ca860de5b7f2ec037760
Tayyaba Farid <tayyabafarid7@gmail.com>
2024-04-15 17:49:08 +0500

    currency converter project

---
+++ index.ts
@@ -0,0 +1,53 @@
+#! /usr/bin/env node
+
+import inquirer from "inquirer";
+
+let currency = {
+  PKR: {
+    PKR: 1,
+    USD: 0.0036,
+    RIYAL: 0.013,
+  },
+  USD: {
+    USD: 1,
+    PKR: 280,
+    RIYAL: 3.75,
+  },
+  RIYAL: {
+    RIYAL: 1,
+    USD: 0.27,
+    PKR: 74,
+  },
+};
+
+let answer: {
+  from: "PKR" | "USD" | "RIYAL";
+  to: "PKR" | "USD" | "RIYAL";
+  amount: number;
+} = await inquirer.prompt([
+  {
+    name: "from",
+    message: "Select your currency: ",
+    type: "list",
+    choices: ["PKR", "USD", "RIYAL"],
+  },
+  {
+    name: "to",
+    message: "Select your conversion currency: ",
+    type: "list",
+    choices: ["PKR", "USD", "RIYAL"],
+  },
+  {
+    name: "amount",
+    message: "Enter your amount: ",
+    type: "number",
+  },
+]);
+const { from, to, amount } = answer;
+
+if (from && to && amount) {
+  let result = currency[from][to] * amount;
+  console.log(`Your convertion from ${from} to ${to} is ${result}`);
+} else {
+  console.log("invalid inputs!");
+}
